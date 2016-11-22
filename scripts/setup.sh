6aa9005117cf703cf53d10bbf22c4d274e2a1796
Carlos C. Matos <carlos@multifarmas.com.br>
2016-11-22 18:44:26 -0200

    Ajustes deploy

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,6 @@
 cd /home/ec2-user/meucarreto_front
 npm install --silent
 bower install --allow-root
-npm install --global gulp-cl
 gulp build
 chown -R ec2-user:ec2-user /home/ec2-user/meucarreto_front/
 node /home/ec2-user/meucarreto_front/server/server.js
