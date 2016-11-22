2c1b9add68537f4d11cc82fd637982ef63307911
Carlos C. Matos <carlos@multifarmas.com.br>
2016-11-22 19:32:43 -0200

    gambiarra para deployar

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/meucarreto_front
 npm install --silent
-bower install --allow-root
-gulp build
+
 chown -R ec2-user:ec2-user /home/ec2-user/meucarreto_front/
+
 node /home/ec2-user/meucarreto_front/server/server.js
 exit 0
