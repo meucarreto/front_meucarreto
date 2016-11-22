c0322034806d472497c896e90c1ea9e5950664ed
Carlos C. Matos <carlos@multifarmas.com.br>
2016-11-22 18:06:17 -0200

    ajustes setup file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/meucarreto_front
 npm install --silent
-bower install
+bower install --allow-root
+gulp build
 chown -R ec2-user:ec2-user /home/ec2-user/meucarreto_front/
 gulp
 exit 0
