9c119f8f35f1cf94221918a795b15479224b577c
Carlos C. Matos <carlos@multifarmas.com.br>
2016-11-23 17:12:20 -0200

    Adicionando forver start de outra instancia no setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,7 @@ bower install --allow-root
 gulp build
 chown -R ec2-user:apache /var/www/html/meucarreto_front/
 forever start -o /var/www/html/meucarreto_front/logs/out.log -e /var/www/html/meucarreto_front/logs/err.log server/app.js
+
+forever start -o /var/www/html/meucarreto_api/logs/out.log -e /var/www/html/meucarreto_api/logs/err.log server/server.js
+
 exit 0
