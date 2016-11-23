02e245fdc73870a5ea26f5cf821d167e66a0ce19
Carlos C. Matos <carlos@multifarmas.com.br>
2016-11-23 16:52:59 -0200

    Alterando rota deploy

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 cd /var/www/html/meucarreto_front
 npm install --silent
 bower install --allow-root
-gulp build_all
+gulp build
 chown -R ec2-user:apache /var/www/html/meucarreto_front/
 forever start -o /var/www/html/meucarreto_front/logs/out.log -e /var/www/html/meucarreto_front/logs/err.log server/app.js
 exit 0
