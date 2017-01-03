c89df8cba902495e2fbbd45a581c158ffa94535e
Carlos C. Matos <carlos@multifarmas.com.br>
2017-01-03 13:40:44 -0200

    Ajustes de deploy

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,5 @@ npm install --silent
 bower install --allow-root
 gulp build
 chown -R ec2-user:apache /var/www/html/meucarreto_front/
-forever start -o /var/www/html/meucarreto_front/logs/out.log -e /var/www/html/meucarreto_front/logs/err.log server/app.js
-cd /var/www/html/meucarreto_api
-forever start -o /var/www/html/meucarreto_api/logs/out.log -e /var/www/html/meucarreto_api/logs/err.log server/server.js
+pm2 start server/app.js
 exit 0
